19c9c800bebd0f01e62be083f2f1a581ce5f1ba3
Berau30 <beraugrib2@gmail.com>
2025-05-22 20:35:51 +0000

    Realizei o nivel mestre estão todos ai separados

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,5 +1,10 @@
-#include <stdio.h>
+    #include <stdio.h>
 
+#define TamanhoDoTabuleiro 10 
+#define HabilidadesNoTabuleiro 5  
+#define LetrasLinhas 10
+#define AguaLinha 10
+#define AguaColuna 10
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
@@ -41,12 +46,7 @@ int main() {
         printf("\n");
     }
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    #include <stdio.h>
-
-#define LetrasLinhas 10
-#define AguaLinha 10
-#define AguaColuna 10
-
+    
     char letras[LetrasLinhas] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
     int agua[AguaLinha][AguaColuna];
 
@@ -92,25 +92,98 @@ int main() {
     }
 
     // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+int aplicarHabilidade(int tabuleiro[TamanhoDoTabuleiro][TamanhoDoTabuleiro], int habilidade[HabilidadesNoTabuleiro][HabilidadesNoTabuleiro], int origemLinha, int origemColuna) {
+    for (int i = 0; i < HabilidadesNoTabuleiro; i++) {
+        for (int j = 0; j < HabilidadesNoTabuleiro; j++) {
+            int linha = origemLinha + i - HabilidadesNoTabuleiro / 2;
+            int coluna = origemColuna + j - HabilidadesNoTabuleiro / 2;
+
+            if (linha >= 0 && linha < TamanhoDoTabuleiro && coluna >= 0 && coluna < TamanhoDoTabuleiro) {
+                if (habilidade[i][j] == 1) {
+                    if (tabuleiro[linha][coluna] == 3) {
+                        tabuleiro[linha][coluna] = 5; 
+                    } else if (tabuleiro[linha][coluna] == 0) {
+                        tabuleiro[linha][coluna] = 1; 
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+int main() {
+    char letras[TamanhoDoTabuleiro] = {'A','B','C','D','E','F','G','H','I','J'};
+    int agua[TamanhoDoTabuleiro][TamanhoDoTabuleiro];
+
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    for (int i = 0; i < TamanhoDoTabuleiro; i++)
+        for (int j = 0; j < TamanhoDoTabuleiro; j++)
+            agua[i][j] = 0;
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    
+    for (int i = 0; i < 3; i++) agua[4][1 + i] = 3;
+    for (int i = 0; i < 3; i++) agua[7 + i][8] = 3;
+    for (int i = 0; i < 3; i++) agua[i][4 - i] = 3;
+    for (int i = 0; i < 3; i++) agua[8 - i][4 - i] = 3;
+
+    
+    int habilidadeCone[HabilidadesNoTabuleiro][HabilidadesNoTabuleiro];
+    for (int i = 0; i < HabilidadesNoTabuleiro; i++) {
+        for (int j = 0; j < HabilidadesNoTabuleiro; j++) {
+            if (j >= (HabilidadesNoTabuleiro - 1) / 2 - i && j <= (HabilidadesNoTabuleiro - 1) / 2 + i)
+                habilidadeCone[i][j] = 1;
+            else
+                habilidadeCone[i][j] = 0;
+        }
+    }
+
+    
+    int habilidadeCruz[HabilidadesNoTabuleiro][HabilidadesNoTabuleiro];
+    for (int i = 0; i < HabilidadesNoTabuleiro; i++) {
+        for (int j = 0; j < HabilidadesNoTabuleiro; j++) {
+            if (i == HabilidadesNoTabuleiro / 2 || j == HabilidadesNoTabuleiro / 2)
+                habilidadeCruz[i][j] = 1;
+            else
+                habilidadeCruz[i][j] = 0;
+        }
+    }
+
+    
+    int habilidadeOctaedro[HabilidadesNoTabuleiro][HabilidadesNoTabuleiro];
+    int centro = HabilidadesNoTabuleiro / 2;
+    for (int i = 0; i < HabilidadesNoTabuleiro; i++) {
+        for (int j = 0; j < HabilidadesNoTabuleiro; j++) {
+            int distLinha = (i > centro) ? (i - centro) : (centro - i);
+            int distColuna = (j > centro) ? (j - centro) : (centro - j);
+            if (distLinha + distColuna <= centro)
+                habilidadeOctaedro[i][j] = 1;
+            else
+                habilidadeOctaedro[i][j] = 0;
+        }
+    }
+
+    
+    aplicarHabilidade(agua, habilidadeCone, 2, 2);
+    aplicarHabilidade(agua, habilidadeCruz, 5, 5);
+    aplicarHabilidade(agua, habilidadeOctaedro, 7, 2);
+
+    
+    printf("  ");
+    for (int i = 0; i < TamanhoDoTabuleiro; i++) printf("%c ", letras[i]);
+    printf("\n");
+
+    for (int i = 0; i < TamanhoDoTabuleiro; i++) {
+        printf("%d ", i);
+        for (int j = 0; j < TamanhoDoTabuleiro; j++) {
+            printf("%d ", agua[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
     return 0;
 }
